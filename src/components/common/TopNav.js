51d7e7ab424a80df05e927cae077b7f83bcf681c
gblessylva <gblessylva@gmail.com>
2020-07-13 05:53:10 +0100

    feat: navigation menu added

--- src/components/common/TopNav.js
+++ src/components/common/TopNav.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import { IoIosSearch } from 'react-icons/io'
+import { FiBell } from 'react-icons/fi'
+import avatar from '../../static/images/avatar.png'
 function TopNav() {
   return (
     <div className='top-nav' >
@@ -8,11 +10,26 @@ function TopNav() {
       </div>
       <div className='search-form'>
         <div>
-          <button type='submit'>Search</button>
+          <button type='submit'><IoIosSearch /></button>
         </div>
-
         < div >
-          <input />
+          <input type='text' placeholder='Seach...' />
+        </div>
+      </div>
+      <div className='links'>
+        <ul>
+          <li> <a href='#'>Support </a> </li>
+          <li> <a href='#'>FAQ </a> </li>
+          <li className='notification'><div className='notify-count'>8</div><FiBell />   </li>
+        </ul>
+        <div className='profile'>
+          <div className='profile-info'>
+            <p className='greetings'>Hello</p>
+            <h3>Oluwaleke Ojo</h3>
+          </div>
+          <div className='profile-avatar'>
+            <img src={avatar} alt='avatar' />
+          </div>
         </div>
       </div>
     </ div>
